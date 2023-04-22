d273d3f6acad5f1deae491cb8da91afe0f023292
evangineer <mamading@gmail.com>
2023-04-22 23:17:16 +0100

    Added dummy React frontend.

---
+++ frontend/input.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+interface InputSpec {
+  name: string;
+  description?: string;
+}
+
+interface FlowSpec {
+  title: string;
+  description: string;
+  inputs: InputSpec[];
+}
+
+const landingPageFlowSpec: FlowSpec = {
+  title: 'Landing Page Generator',
+  description:
+    "Give us a few details about your product and we'll generate a simple email collection landing page for you",
+  inputs: [
+    {
+      name: 'Product name',
+    },
+    {
+      name: 'Industry',
+    },
+    {
+      name: 'Description',
+      description:
+        'A brief description about your offering, feel free to use bullet points',
+    },
+  ],
+};
+
+const LandingPageForm: React.FC = () => {
+  const [formValues, setFormValues] = useState({});
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+
+  return (
+    <div>
+      <h1>{landingPageFlowSpec.title}</h1>
+      <p>{landingPageFlowSpec.description}</p>
+      <form onSubmit={handleSubmit}>
+        {landingPageFlowSpec.inputs.map((input, index) => (
+          <div key={index}>
+            <label htmlFor={input.name}>{input.name}</label>
+            <input
+              type="text"
+              id={input.name}
+              name={input.name}
+              placeholder={input.description}
+              onChange={handleChange}
+            />
+          </div>
+        ))}
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default LandingPageForm;
