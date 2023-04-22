7e9e41697aea1c5066fcf4cf818e7b905f34cb79
Danny Hunt <dansinh64@gmail.com>
2023-04-22 21:54:44 +0100

    rename userinput

--- src/bin/promptNode.ts
+++ src/bin/promptNode.ts
@@ -1,7 +1,7 @@
 
 
 // Represents an input to a node that will be a text field for the end-user page
-class Input {
+class UserInput {
   name: string;
   value: string;
 }
@@ -11,26 +11,21 @@ class PromptNode {
   id: string;
   title: string;
   children: PromptNode[];
-  inputs: Input[];
+  inputs: UserInput[];
   model: 'cha'
   parent: PromptNode;
   promptTemplate: string;
   stopCondition?: string; // If we use LMQL
 
 
-  constructor(title: string, inputs: Input[], children: PromptNode[], parent: PromptNode, promptTemplate: string, stopCondition?: string) {
+  constructor(title: string, inputs: UserInput[], children: PromptNode[], parent: PromptNode, promptTemplate: string, stopCondition?: string) {
     this.id = crypto.randomUUID();
-  }
-
-  // Frontend only to gather inputs
-  listAllInputs () {
-
   }
   
   // For backend to run the node
-  async run(input: string) {
+  async run(previousStepOutput: string) {
     // run prompt to get outputs
-    const output = "INSERT PROMPT RESULT HERE"
+    const output = previousStepOutput ? "INSERT PROMPT RESULT HERE" : "foo"
 
     // run stop condition to see if we should stop(?)
     if (this.stopCondition)
@@ -43,10 +38,10 @@ class PromptNode {
 
 // Hopefully returns all inputs that exist in the flow
 // Takes the rootnode as its first argument and a new empty set as its second argument
-const listInputsForFlow = (flow: PromptNode, seenIds: Set<string>): Input[] => {
+const listUserInputsForFlow = (flow: PromptNode, seenIds: Set<string>): UserInput[] => {
   if (seenIds.has(flow.id)) {
     return []
   }
   seenIds.add(flow.id)
-  return [...flow.inputs, ...flow.children.flatMap(child => listInputsForFlow(child, seenIds))]
+  return [...flow.inputs, ...flow.children.flatMap(child => listUserInputsForFlow(child, seenIds))]
 }
