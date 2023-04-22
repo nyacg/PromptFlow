27dc2624ca171f9b30daddedd2d2dde703fac364
Danny Hunt <dansinh64@gmail.com>
2023-04-22 22:24:43 +0100

    add dotenv for managing api key

--- src/bin/main.ts
+++ src/bin/main.ts
@@ -1,1 +1,9 @@
+import * as dotenv from 'dotenv';
+// I still need to install dotenv as a dependency -- Danny
+
+dotenv.config();
+const OPEN_API_KEY = process.env.OPEN_API_KEY;
+
+const rootNode = getInitialNode();
+
 console.log("Hello world")
