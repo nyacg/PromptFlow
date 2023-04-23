003408ac405c72899043f3e921e7365729cbd725
Danny Hunt <dansinh64@gmail.com>
2023-04-23 01:09:39 +0100

    tmp

---
+++ pf-frontend/components/FlowGraph.tsx
@@ -0,0 +1,56 @@
+import React, { useCallback } from 'react';
+import ReactFlow, {
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+} from 'reactflow';
+
+import { initialEdges, initialNodes } from './initial-elements';
+
+const minimapStyle = {
+  height: 120,
+};
+
+const onInit = (reactFlowInstance: any) => console.log('flow loaded:', reactFlowInstance);
+
+export const FlowGraph = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState<{label: string}>(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState<{label: string}>(initialEdges);
+  const onConnect = useCallback((params: any) => setEdges((eds) => addEdge(params, eds)), []);
+
+  // we are using a bit of a shortcut here to adjust the edge type
+  // this could also be done with a custom edge for example
+  const edgesWithUpdatedTypes = edges.map((edge) => {
+    if (edge.sourceHandle) {
+        //@ts-ignore this was in the library example so I hope it's ok
+      const edgeType = nodes.find((node) => node.type === 'custom').data.selects[edge.sourceHandle];
+      edge.type = edgeType;
+    }
+
+    return edge;
+  });
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+      nodes={nodes}
+      edges={edgesWithUpdatedTypes}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      onInit={onInit}
+      fitView
+      attributionPosition="top-right"
+      >
+        <MiniMap style={minimapStyle} zoomable pannable />
+        <Controls />
+        <Background color="#fff" gap={16} />
+      </ReactFlow>
+    </div>
+  );
+};
+
+export default FlowGraph;
---
+++ pf-frontend/components/initial-elements.ts
@@ -0,0 +1,32 @@
+
+
+export const initialNodes = [
+  {
+    id: '1',
+    type: 'input',
+    data: {
+      label: 'Input Node',
+    },
+    position: { x: 250, y: 0 },
+  },
+  {
+    id: '2',
+    data: {
+      label: 'Default Node',
+    },
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: '3',
+    type: 'output',
+    data: {
+      label: 'Output Node',
+    },
+    position: { x: 400, y: 100 },
+  },
+];
+
+export const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2', label: 'this is an edge label' },
+  { id: 'e1-3', source: '1', target: '3', animated: true },
+];
