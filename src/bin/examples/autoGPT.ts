257a36afb9ebae40bfd3396c39f82b13bb486242
Danny Hunt <dansinh64@gmail.com>
2023-04-23 14:47:38 +0100

    fix autogpt example but at what cost

--- src/bin/examples/autoGPT.ts
+++ src/bin/examples/autoGPT.ts
@@ -22,7 +22,8 @@ Task 7: {{task7}}
 Task 8: {{task8}}
 Task 9: {{task9}}
 Task 10: {{task10}}`,
-  children: []
+  children: [],
+  final: true
 })
 
 
@@ -33,7 +34,7 @@ export const autoGPT = (): PromptNode => {
     inputs: [
       {
         name: "goal",
-        value: "I want to win a London-based hackathon that's centred around used foundation models (LLMs)",
+        value: "I want to win a London-based hackathon that's centred around using foundation models (LLMs)",
       },
     ],
     output: new ListOutput(N, "initialtasks"),
@@ -42,24 +43,41 @@ List the ${N} steps I need to take to achieve this goal.
 For instance, {{expert}} would answer:`,
     children: [
       new PromptNode({
-        title: "Rank tasks",
+        title: "Prioritise tasks",
         expectedNumberOfParentOutputs: 1,
         inputs: [
           {
             name: "goal",
-            value: "I want to win a London-based hackathon that's centred around used foundation models (LLMs)",
+            value: "I want to win a London-based hackathon that's centred around using foundation models (LLMs)",
           }
         ],
         output: new ListOutput(N, "rankedtasks"),
-        promptTemplate: `Q: order these tasks in the order in which I should complete them to maximise my chances of achieving this goal: {{goal}}
-{{initialtasks}}`,
+        promptTemplate: `Q: re-order these tasks in the chronological order in which I should complete them to maximise my chances of achieving this goal: {{goal}}.
+Tasks: 
+1: {{initialtasks1}}
+2: {{initialtasks2}}
+3: {{initialtasks3}}
+4: {{initialtasks4}}
+5: {{initialtasks5}}
+6: {{initialtasks6}}
+7: {{initialtasks7}}
+8: {{initialtasks8}}
+9: {{initialtasks9}}
+10: {{initialtasks10}}`,
         children: 
           times(N).map((i) => new PromptNode({
             title: "Complete task",
             expectedNumberOfParentOutputs: 1,
             inputs: [],
             output: new Output(`task${i + 1}`),
-            promptTemplate: `Q: complete this task: {{rankedtasks${i}}}`, // TODO - make sure this works with the fanning implementation from promptNode.ts
+            promptTemplate: `Imagine that you have been a human working on a team in a hackathon.
+Pretend to have completed this specific task: {{rankedtasks${i}}}.
+Hackathon organiser: Hey how have you been doing?
+You: I've been doing this: {{rankedtasks${i}}}.
+Hackathon organiser: Did you complete that task?
+You: Yes!
+Hackathon organiser: Explain how you completed that specific task and whether there were any unexpected events along the way
+You:`,
             children: [
               aggregatingNode
             ]
